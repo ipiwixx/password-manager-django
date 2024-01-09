80db19ba9c5f00718d1b582328e6430ecd1acfba
ipiwixx <124789918+ipiwixx@users.noreply.github.com>
2024-01-09 16:13:35 +0100

    Création de model Site

--- passwordManager/passworManagerApp/models.py
+++ passwordManager/passworManagerApp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Site(models.Model):
+    nom = models.CharField(max_length=200)
+    url = models.CharField(max_length=300)
+    identifiant = models.CharField(max_length=200)
+    password = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.nom
